06bd5b49c984a536bd295be9c587b22d14f5479b
limyeonsang <limquo@gmail.com>
2022-09-05 20:07:37 +0900

    post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,6 +20,17 @@ def show(id: int):
   return {"data": id}
 
 
-@app.get("blog/{id}/comments")
-def comments(id):
+@app.get("/blog/{id}/comments")
+def comments(id, limit=10):
   return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+  title: str
+  body: str
+  published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+  return {"data": f"Blog created by {request.title}"}
