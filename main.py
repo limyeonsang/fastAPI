5ba4d6987049c9beb525a7a31c4b320484cfbc25
limyeonsang <limquo@gmail.com>
2022-09-05 19:22:43 +0900

    path params

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ app = FastAPI()
 def index():
   return {"data": {"name":"yeonsang"}}
 
-@app.get("/about")
-def about():
-  return {"data": ["about page"]}
+@app.get("/blog/unpublished")
+def unpublished():
+  return {"data": "all unpublished blogs"}
+  
+@app.get("/blog/{id}")
+def show(id: int):
+  return {"data": id}
+
+
+@app.get("blog/{id}/comments")
+def comments(id):
+  return {"data": {"1", "2"}}
