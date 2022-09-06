94817c79a21246d363419c85035f15c3a674bf03
limyeonsang <limquo@gmail.com>
2022-09-06 15:30:49 +0900

    add user

--- blog/main.py
+++ blog/main.py
@@ -70,3 +70,13 @@ def single_blog(id, response: Response, db: Session = Depends(get_db)):
         # return {'details': f'incorrect id {id}'}
     return blog
 
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
