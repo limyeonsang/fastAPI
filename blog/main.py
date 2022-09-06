17336c73725f900eeb578154016190482a9d94c0
limyeonsang <limquo@gmail.com>
2022-09-06 11:20:03 +0900

    status 404

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from urllib import response
+from fastapi import FastAPI, Depends, status, Response
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -16,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=201)
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -33,8 +34,10 @@ def all_blog(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blog/{id}')
-def single_blog(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def single_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'details': f'incorrect id {id}'}
     return blog
