3d60da933a31aefb5b58cffb44b42657cb9fbf42
limyeonsang <limquo@gmail.com>
2022-09-06 11:07:55 +0900

    get blog

--- blog/main.py
+++ blog/main.py
@@ -24,3 +24,17 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
 
     return new_blog
+
+
+@app.get('/blog')
+def all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+
+    return blogs
+
+
+@app.get('/blog/{id}')
+def single_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    return blog
