d683d5d39fc002a97f9903032501c7a2ca55c2df
limyeonsang <limquo@gmail.com>
2022-09-06 14:56:10 +0900

    update

--- blog/main.py
+++ blog/main.py
@@ -26,6 +26,32 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def del_post(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'incorret id {id}')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'complete'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'incorrect id {id}')
+
+    blog.update(request.dict())
+    db.commit()
+    return 'update success'
+
+
 @app.get('/blog')
 def all_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
